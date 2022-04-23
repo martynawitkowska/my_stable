8c6de1b87ba3e6b40a7c8f78fcad71dc81dad0f4
Martyna Witkowska <mar.witkowska@gmail.com>
2022-04-23 18:35:41 +0200

    edited horse and stable table to add slug filed, to feeding added date_created field, created part of horse detail view

--- horses/urls.py
+++ horses/urls.py
@@ -7,5 +7,7 @@ app_name = 'horses'
 urlpatterns = [
     path('add-horse', views.add_horse_view, name='add_horse'),
     path('add-stable', views.add_stable_view, name='add_stable'),
-    path('stable/<int:user_id>', views.StableView.as_view(), name='stable')
+    path('stable/<int:user_id>', views.StableView.as_view(), name='stable'),
+    path('add-meal', views.AddMealPlan.as_view(), name='add_meal'),
+    path('<slug:slug>', views.HorseDetailView.as_view(), name='horse_detail'),
 ]
