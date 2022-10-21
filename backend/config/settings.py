4e4a97cc52c5554ca9c3ae62cf4440b7c5410244
Martyna Witkowska <mar.witkowska@gmail.com>
2022-10-21 18:11:51 +0200

    add todo, import environ

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -13,6 +13,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+# TODO import environ
 import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
