7662776434372f1156a8c8afaedac79a1ab3d9a9
Martyna Witkowska <mar.witkowska@gmail.com>
2022-10-21 18:10:07 +0200

    add entrypoint

---
+++ backend/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if test "$POSTGRES_DB" = "my_stable"
+then
+  echo "Waiting for postgres"
+
+  while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+    sleep 0.1
+  done
+
+  echo "PostgreSQL started"
+
+fi
+
+python manage.py flush --no_input
+python manage.py migrate
+
+exec "$@"
